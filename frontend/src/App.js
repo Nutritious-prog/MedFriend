76cc221b3a84e6cc93262c42e416a0868292f305
nutritious-prog <artur.wlodarczyk12@gmail.com>
2022-09-18 17:26:46 +0200

    AllInOne section finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About';
+import AllInOne from './components/AllInOne';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Support from './components/Support';
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Support />
+      <AllInOne />
     </>
   );
 }
